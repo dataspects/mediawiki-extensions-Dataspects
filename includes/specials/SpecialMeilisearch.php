da3cc3428289003e7ad4ae942c3ec071850fd9d2
Lex Sulzer <lex@dataspects.com>
2022-06-28 12:03:54 +0200

    init

--- includes/specials/SpecialMeilisearch.php
+++ includes/specials/SpecialMeilisearch.php
@@ -1,11 +1,33 @@
 <?php
 
+use MeiliSearch\Client;
+
+
 class SpecialMeilisearch extends SpecialPage {
 	function __construct() {
 		parent::__construct( 'Meilisearch' );
 	}
 
 	function execute( $par ) {
+
+		echo $par["a"];
+
+		$client = new Client('http://192.168.1.36:7700', 'masterKey');
+
+# An index is where the documents are stored.
+$index = $client->index('movies');
+
+$hits = $index->search('wondre woman')->getHits();
+
+
+
+
+
+
+
+
+
+
 		$request = $this->getRequest();
 		$output = $this->getOutput();
 		$this->setHeaders();
@@ -16,6 +38,6 @@ class SpecialMeilisearch extends SpecialPage {
 		# Do stuff
 		# ...
 		$wikitext = 'Hello world!';
-		$output->addWikiTextAsInterface( $wikitext );
+		$output->addWikiTextAsInterface( $hits[2]["genres"][0] );
 	}
 }
--- resources/ext.boilerPlate/init.js
+++ resources/ext.boilerPlate/init.js
@@ -2,4 +2,15 @@
  * @class mw.meilisearchForMediaWiki
  * @singleton
  */
-mw.meilisearchForMediaWiki = {};
+import { instantMeiliSearch } from "@meilisearch/instant-meilisearch";
+
+const searchClient = instantMeiliSearch(
+  "http://192.168.1.36:7700",
+  "masterKey"
+);
+
+console.debug("test");
+
+mw.meilisearchForMediaWiki = {
+  a: "b",
+};
