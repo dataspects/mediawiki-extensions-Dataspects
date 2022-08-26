d4d3d64d18e079ea0826537a59daeb41d79fa262
Lex Sulzer <lex@dataspects.com>
2022-08-26 09:54:59 +0200

    backstage_table

--- includes/specials/SpecialDataspectsSearch.php
+++ includes/specials/SpecialDataspectsSearch.php
@@ -10,22 +10,35 @@ class SpecialDataspectsSearch extends SpecialPage {
 		$output = $this->getOutput();
 		$this->setHeaders();
 
-		$output->addWikiTextAsContent( '{|class="backstage_table"
-			|wgDataspectsSearchWriteURL
-			|'.$GLOBALS['wgDataspectsSearchWriteURL'].'
-			|-
-			|wgDataspectsSearchSearchURL
-			|'.$GLOBALS['wgDataspectsSearchSearchURL'].'
-			|-
-			|wgDataspectsSearchIndex
-			|'.$GLOBALS['wgDataspectsSearchIndex'].'
-			|}' );
-
 		$output->addHTML( '<table class="dataspectsSearchInterface">
 			<tr>
 				<td colspan=2>
 					<div id="searchbox"></div>
 					<a href="'.$GLOBALS['wgServer'].'/wiki/Special:DataspectsSearch">Reset</a>
+				</td>
+				<td>
+					<table class="backstage_table">
+						<tbody><tr>
+						<td>wgDataspectsSearchWriteURL
+						</td>
+						<td>'.$GLOBALS['wgDataspectsSearchWriteURL'].'
+						</td></tr>
+						<tr>
+						<td>wgDataspectsSearchSearchURL
+						</td>
+						<td>'.$GLOBALS['wgDataspectsSearchSearchURL'].'
+						</td></tr>
+						<tr>
+						<td>wgDataspectsSearchIndex
+						</td>
+						<td>'.$GLOBALS['wgDataspectsSearchIndex'].'
+						</td></tr>
+						<tr>
+						<td>wgDataspectsSearchTikaURL
+						</td>
+						<td>'.$GLOBALS['wgDataspectsSearchTikaURL'].'
+						</td></tr></tbody>
+					</table>
 				</td>
 			</tr>
 			<tr>
