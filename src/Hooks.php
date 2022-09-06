696b5d8fc02d4b20a74dd0ab7a4fb8b951f5dac0
Lex Sulzer <lex@dataspects.com>
2022-09-06 12:18:09 +0200

    onPageSaveComplete working

--- src/Hooks.php
+++ src/Hooks.php
@@ -1,21 +1,4 @@
 <?php
-/**
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * @file
- */
 
 namespace MediaWiki\Extension\DataspectsSearch;
 
@@ -23,22 +6,20 @@ use ManualLogEntry;
 use Title;
 use User;
 
-class Hooks implements \MediaWiki\Hook\BeforePageDisplayHook {
+class Hooks implements \MediaWiki\Storage\Hook\PageSaveCompleteHook {
 
-	/**
-	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/BeforePageDisplay
-	 * @param \OutputPage $out
-	 * @param \Skin $skin
-	 */
-	public function onBeforePageDisplay( $out, $skin ): void {
+	
+	public function onPageSaveComplete( $wikiPage, $user, $summary, $flags, $revisionRecord, $editResult ) {
 		// https://www.mediawiki.org/wiki/Manual:Logging_to_Special:Log
 		$logEntry = new ManualLogEntry( 'dataspects', 'test' );
-		$logEntry->setTarget( Title::newFromText( "sad" ) ); #FIXME?
-		$logEntry->setPerformer( User::newSystemUser("performer") ); #FIXME?
+		$logEntry->setTarget( $wikiPage->getTitle() );
+		$logEntry->setPerformer( $user );
 		$logEntry->setParameters( [
-			'4::unused' => 'log test',
+			'4::unused' => 'log onPageSaveComplete: '.$wikiPage->getTitle()->getBaseTitle(). " by ".$user->getName(),
 		] );
 		$logEntry->insert();
 	}
+
+
 
 }
