07fd29636f28bb2ffab5b85df424a784808932c3
Lex Sulzer <lex@dataspects.com>
2022-11-09 11:23:36 +0100

    No more risky tests

--- tests/phpunit/unit/DataspectsTest.php
+++ tests/phpunit/unit/DataspectsTest.php
@@ -3,7 +3,6 @@
 # https://www.mediawiki.org/wiki/Manual:PHP_unit_testing/Writing_unit_tests_for_extensions
 
 namespace MediaWiki\Extension\Dataspects;
-use MediaWiki\MediaWikiServices;
 
 class DataspectsTest extends \MediaWikiUnitTestCase {
 
@@ -15,10 +14,13 @@ class DataspectsTest extends \MediaWikiUnitTestCase {
 		parent::tearDown();
 	}
 
+	/**
+	 * @covers MediaWiki\Extension\Dataspects\TextAccounting
+	 */
 	public function testRenderNumberOfWords() {
 		$str = "We are learning how to code.";
 		$output = TextAccounting::numberOfWords($str);
-		echo "\nEcho info like this...\n";
+		// echo "\nEcho info like this...\n";
 		$this->assertEquals( $output, "<i>$str</i> contains 6 words." );
 	}
 }
